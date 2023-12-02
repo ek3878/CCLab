837010715a0ad1ae075f2cf526d4b851a184ed77
ek3878 <ek3878@nyu.edu>
2023-12-03 04:47:12 +0800

    canvas1

--- project-b-draft/canvas1/sketch.js
+++ project-b-draft/canvas1/sketch.js
@@ -1,3 +1,98 @@
+// //let mcqueen, sheriff, sally;
+// let x, y;
+// let img = [];
+// let cars = [];
+// let counter = 0;
+// let choose = true;
+
+// function preload() {
+//   img[0] = loadImage("maqueen.png");
+//   img[1] = loadImage("sheriff.png");
+//   img[2] = loadImage("sally.png");
+//   bg = loadImage("bg.jpeg");
+// }
+
+// function setup() {
+//   let canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.parent("canvasContainer");
+
+//   // createCanvas(600, 400);
+
+//   x = windowWidth / 2 - 240;
+//   y = windowHeight / 2 + 30;
+
+//   for (let i = 0; i < img.length; i++) {
+//     cars[i] = new Car(x, y, i);
+//   }
+// }
+
+// function draw() {
+//   //background(0);
+//   image(bg, 0, 0, windowWidth, windowHeight);
+//   //button
+//   fill(255);
+//   rect(windowWidth - 240, windowHeight/2 - 100, 150, 200);
+//   rect(80, windowHeight/2 - 100, 150, 200);
+
+//   stage1();  
+
+//   if (keyCode == ENTER) {
+//     choose = false;
+//     image(bg, 0, 0, windowWidth, windowHeight);
+//     stage2();
+//     storeItem("counter", counter);
+//   }
+//   //console.log(counter);
+// }
+
+// function stage1() {
+//   if (counter >= img.length) {
+//     counter = 0;
+//   }
+//   if (counter < 0) {
+//     counter = img.length - 1;
+//   }
+
+//   cars[counter].display();
+// }
+
+// function stage2() {
+//   cars[counter].display();
+//   cars[counter].move();
+// }
+
+// function mousePressed() {
+//   if (
+//     mouseX > windowWidth - 240 &&
+//     mouseX < windowWidth - 90 &&
+//     mouseY > windowHeight/2 - 100 &&
+//     mouseY < windowHeight/2 + 100
+
+//   ) {
+//     counter = counter + 1;
+//   }
+//   if (mouseX > 80 && mouseX < 230 && mouseY > windowHeight/2 - 100 && mouseY < windowHeight/2 + 100) {
+//     counter = counter - 1;
+//   }
+// }
+
+// class Car {
+//   constructor(x, y, index) {
+//     this.x = x;
+//     this.y = y;
+//     this.img = img[index];
+//   }
+//   display() {
+//     image(this.img, this.x, this.y, windowWidth*0.3, windowHeight*0.25);
+//     //image(this.img, this.x, this.y, 170, 100);
+//   }
+//   move() {
+//     if (this.x < width) {
+//       this.x += 5;
+//     }
+//   }
+// }
+
 //let mcqueen, sheriff, sally;
 let x, y;
 let img = [];
@@ -13,13 +108,12 @@ function preload() {
 }
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
+  let canvas = createCanvas(1000, 500);
   canvas.parent("canvasContainer");
-
   // createCanvas(600, 400);
 
-  x = windowWidth / 2 - 240;
-  y = windowHeight / 2 + 30;
+  x = width / 2 - 88;
+  y = height / 2 + 30;
 
   for (let i = 0; i < img.length; i++) {
     cars[i] = new Car(x, y, i);
@@ -27,18 +121,17 @@ function setup() {
 }
 
 function draw() {
-  //background(0);
-  image(bg, 0, 0, windowWidth, windowHeight);
+  image(bg, 0, 0, width, height);
   //button
   fill(255);
-  rect(windowWidth - 240, windowHeight/2 - 100, 150, 200);
-  rect(80, windowHeight/2 - 100, 150, 200);
+  rect(width*0.88, height/2 + 20, 50, 50);
+  rect(width*0.065, height/2 + 20, 50, 50);
 
-  stage1();  
+  stage1();
 
   if (keyCode == ENTER) {
     choose = false;
-    image(bg, 0, 0, windowWidth, windowHeight);
+    image(bg, 0, 0, width, height);
     stage2();
     storeItem("counter", counter);
   }
@@ -62,17 +155,18 @@ function stage2() {
 }
 
 function mousePressed() {
-  if (
-    mouseX > windowWidth - 240 &&
-    mouseX < windowWidth - 90 &&
-    mouseY > windowHeight/2 - 100 &&
-    mouseY < windowHeight/2 + 100
-
-  ) {
-    counter = counter + 1;
-  }
-  if (mouseX > 80 && mouseX < 230 && mouseY > windowHeight/2 - 100 && mouseY < windowHeight/2 + 100) {
-    counter = counter - 1;
+  if (choose == true) {
+    if (
+      mouseX > width*0.88 &&
+      mouseX < width*0.88+50 &&
+      mouseY > height/2 + 20 &&
+      mouseY < height/2 + 70
+    ) {
+      counter = counter + 1;
+    }
+    if (mouseX > width*0.065 && mouseX < width*0.065+50 && mouseY > height/2 + 20 && mouseY < height/2 + 70) {
+      counter = counter - 1;
+    }
   }
 }
 
@@ -83,12 +177,11 @@ class Car {
     this.img = img[index];
   }
   display() {
-    image(this.img, this.x, this.y, windowWidth*0.3, windowHeight*0.25);
-    //image(this.img, this.x, this.y, 170, 100);
+    image(this.img, this.x, this.y, 170, 100);
   }
   move() {
     if (this.x < width) {
-      this.x += 5;
+      this.x += 2;
     }
   }
 }
